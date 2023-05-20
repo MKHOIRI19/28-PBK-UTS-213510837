642c350afd519ea87650282990dc8946dc78431a
MKHOIRI19 <mkhoiri@student.uir.ac.id>
2023-05-20 15:33:12 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="black">{{ msg }}</h1>
+    <h3>
+      <a href="https://instagram.com/sembilan.boleh19?igshid=ZGUzMzM3NWJiOQ==" target="_blank" rel="noopener"><img src="@/assets/IG.png" alt=""></a> &
+      <a href="https://github.com/MKHOIRI19/28-PBK-UTS-213510837" target="_blank" rel="noopener"><img src="@/assets/github.png" alt=""></a>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+img{
+  width: 8%;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1> DATA DIRI</h1>
+    <p align="right">Nama Lengkap : M.KHOIRI</p><br>
+    <p align="right">NPM : 213510837</P><br>
+    <p align="right">TTL : Duri, 19 JULY 2002</p>
+
+    <h2>TEKNIK INFORMATIKA</h2>
+    <h1>UNIVERSITAS ISLAM RIAU</h1>
+  </main>
+</template>
+
+<style>
+h1 {
+    text-align: center;
+    font-size: 25px;
+    font-weight: bold;
+    margin: 20px 0;
+    text-transform: uppercase;
+    color: black;
+}
+
+h2{
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 20px 0;
+    text-transform: uppercase;
+    color: black;
+}
+
+p {
+    text-align: center;
+    font-size: 18px;
+    margin: auto;
+    color: black;
+}
+
+</style>
